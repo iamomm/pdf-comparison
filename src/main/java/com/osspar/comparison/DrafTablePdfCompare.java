ba895a57e5de0b39cd9bf1c12702e6fbf49d6857
Omprakash <omprakashm92@gmail.com>
2023-08-11 14:26:59 +0530

    Fixed Url fetching

--- src/main/java/com/osspar/comparison/DrafTablePdfCompare.java
+++ src/main/java/com/osspar/comparison/DrafTablePdfCompare.java
@@ -23,63 +23,60 @@ public class DrafTablePdfCompare {
     String newImageName = "Individual_View_New.jpg";
 
     public static void main(String[] args) {
+        //System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.client.protocol.ResponseProcessCookies", "fatal");
+        //System.setProperty("draftable.allowSelfSignedCerts", "1");
         DrafTablePdfCompare drafTablePdfCompare = new DrafTablePdfCompare();
         drafTablePdfCompare.drafTableCompare();
     }
 
     public void drafTableCompare() {
+        Comparisons comparisons = null;
+
         try {
             String oldImgPath = resources_path + oldPdfName;
             String newImgPath = resources_path + newPdfName;
             File oldFile = new File(oldImgPath);
             File newFile = new File(newImgPath);
-            Comparisons comparisons = new Comparisons("dafCMh-test", "dc53d0761f4624c3c1af5d66c0252234");
-            //Comparisons comparisons = new Comparisons("", "", "http://localhost:4200/");
 
+            comparisons = new Comparisons("dafCMh-test", "dc53d0761f4624c3c1af5d66c0252234");
+
+            //String identifier = Comparisons.generateIdentifier();
             Comparison comparison = comparisons.createComparison(
                     Comparisons.Side.create(oldFile, "pdf", oldPdfName),
                     Comparisons.Side.create(newFile, "pdf", newPdfName)
             );
-            //, null, true, Instant.now().plusSeconds(1200)
-            String comparisonId = comparison.getIdentifier();
-            comparison = comparisons.getComparison(comparisonId);
-
-            System.out.println(String.format("Comparison created: %s", comparison));
+            //Comparison cp = comparison.get();
+            //String comparisonId = comparison.getIdentifier();
 
             //Generate a signed viewer URL to access the private comparison. The expiry
             //time defaults to 30 minutes if the validUntil parameter is not provided.
-            //String viewerURL = comparisons.signedViewerURL(comparison.getIdentifier());
-            //System.out.println(String.format("Viewer URL (expires in 30 mins): %s", viewerURL));
+            String viewerURL = comparisons.signedViewerURL(comparison.getIdentifier());
+            System.out.println(String.format("Viewer URL (expires in 30 mins): %s", viewerURL));
+            Thread.sleep(3000);
 
-            //String viewerURL = comparisons.signedViewerURL(comparison.getIdentifier());
-            //System.out.println(String.format("Public url: %s", viewerURL));
-
-            /*try (BufferedInputStream in = new BufferedInputStream(new URL(viewerURL).openStream());
-                 FileOutputStream fileOutputStream = new FileOutputStream(resources_path + "abcd.pdf")) {
-                byte dataBuffer[] = new byte[1024];
-                int bytesRead;
-                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                    fileOutputStream.write(dataBuffer, 0, bytesRead);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                //handle exception
-            }*/
-
-            Export export = comparisons.createExport(comparisonId, ExportKind.SINGLE_PAGE);
+            Export export = comparisons.createExport(comparison.getIdentifier(), ExportKind.COMBINED, false);
 
             System.out.println(String.format("Post export created id: %s", export.getIdentifier()));
             System.out.println(String.format("Post export created cp: %s", export.getComparison()));
+            System.out.println("Post export created ready --"+ export.isReady());
+            Thread.sleep(2000);
+
             Export getExport = comparisons.getExport(export.getIdentifier());
 
             System.out.println(String.format("exporting data id: %s", getExport.getIdentifier()));
             System.out.println(String.format("exporting data cp: %s", getExport.getComparison()));
             System.out.println(String.format("exporting data ur: %s", getExport.getUrl()));
-            System.out.println(String.format("exporting data ready: %s", getExport.isReady()));
+            System.out.println("exporting data ready: "+ getExport.isReady());
             System.out.println(String.format("exporting data failed: %s", getExport.getFailed()));
             System.out.println(String.format("exporting data failed: %s", getExport.getErrorMessage()));
         } catch (Exception ex) {
             ex.printStackTrace();
+        } finally {
+            try {
+                comparisons.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
